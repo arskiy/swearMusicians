b96164b4a9ec133529c8664d06ef2efdf41f94b5
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2019-02-01 16:49:25 +0500

    Added Graphs

---
+++ graphs.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+artists = ['50_cent', '21_savage', 'B-Real', 'Machine_Gun_Kelly', 'Eminem', 'Snoop_Dogg', 'Nicki Minaj', 'Tyler, The Creator', 'Dj_Khaled', 'Migos', 'Kendrick_Lamar', 'Lil_Peep', 'Lil_Pump', 'Lil_Wayne', 'Lil_Uzi_Vert', 'Soulja_Boy']
+
+data = {'Number of Words': [254388, 31023, 6981, 68818, 393558, 256696, 131489, 44263, 108580, 87143, 84223, 6222, 9190, 521529, 34362, 76853], 'Number of slangs': [7922, 1466, 154, 1575, 10513, 7083, 3430, 1781, 2593, 2500, 2286, 115, 326, 16641, 941, 1805], 'Average of words until a slang': [32, 21, 45, 43, 37, 36, 38, 24, 41, 34, 36, 54, 28, 31, 36, 42]}
+
+df = pd.DataFrame(data, 
+        columns = ["Number of Words","Number of slangs","Average of words until a slang"],
+        index = artists
+        )
+
+wordstoslang = data["Average of words until a slang"]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.barh(artists, wordstoslang)
+labels = [ax.get_xticklabels(),ax.get_yticklabels()]
+plt.setp(labels,fontsize=12)
+
+plt.xlabel("Average words between 2 consecutive swears",fontsize =18)
+plt.title("Comparison of rapper swearing",fontsize = 20)
+plt.show()
+fig.savefig("fig.png",dpi=180,bbox_inches="tight")
+
